da56d9397bab963f55843df49f5cafede2674396
Gautam <gautamhans1@gmail.com>
2017-08-10 20:58:37 +0530

    Wrote Espresso UI Test

---
+++ app/src/androidTest/java/gautamhans/xyz/bakeaid/ui/RecipeActivityTest.java
@@ -0,0 +1,76 @@
+package gautamhans.xyz.bakeaid.ui;
+
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import gautamhans.xyz.bakeaid.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RecipeActivityTest {
+
+    @Rule
+    public ActivityTestRule<RecipeActivity> mActivityTestRule = new ActivityTestRule<>(RecipeActivity.class);
+
+    private IdlingResource mIdlingResource;
+
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
+    @Test
+    public void recipeActivityTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rv_recipes),
+                        withParent(withId(R.id.recipe_fragment)),
+                        isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.exo_pause), withContentDescription("Pause"), isDisplayed()));
+        appCompatImageButton.perform(click());
+
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.exo_play), withContentDescription("Play"), isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.nextStep), withText("Next"), isDisplayed()));
+        appCompatButton.perform(click());
+
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
+    }
+
+}
